1832136e7e831d4b48e8b72993a9945cec3dd439
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2023-05-03 20:03:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -427,7 +427,8 @@ if score >=9 :
     for percent_complete in range(score*10):
         time.sleep(0.02)
         my_bar.progress(percent_complete + 1, text=progress_text)
-    st.text(score*10%)
+    st.text(score*10,'%')
+    
 elif score <=1 :
     progress_text = "Very Low Risk"
     my_bar = st.progress(0, text=progress_text)
@@ -436,7 +437,8 @@ elif score <=1 :
         time.sleep(0.02)
         my_bar.progress(percent_complete + 1, text=progress_text)
 
-    st.text(score*10%)
+    st.text(score*10,'%')
+    
 elif score <=3 and score>=2:
     progress_text = "Low Risk"
     my_bar = st.progress(40, text=progress_text)
@@ -445,7 +447,7 @@ elif score <=3 and score>=2:
         time.sleep(0.02)
         my_bar.progress(percent_complete + 1, text=progress_text)
     
-    st.text(score*10%)  
+    st.text(score*10,'%')  
         
 elif score <=6 and score >=4 :
     progress_text = "Moderate Risk"
@@ -454,7 +456,7 @@ elif score <=6 and score >=4 :
     for percent_complete in range(score*10):
         time.sleep(0.02)
         my_bar.progress(percent_complete + 1, text=progress_text)
-    st.text(score*10%)
+    st.text(score*10,'%')
 elif score <=8 and score >=7 :
     progress_text = "High Risk"
     my_bar = st.progress(80, text=progress_text)
@@ -462,7 +464,7 @@ elif score <=8 and score >=7 :
     for percent_complete in range(score*10):
         time.sleep(0.02)
         my_bar.progress(percent_complete + 1, text=progress_text)
-    st.text(score*10%)
+    st.text(score*10,'%')
 
 st.caption('Based on 10 fundamental aspects of an equity.')
         
